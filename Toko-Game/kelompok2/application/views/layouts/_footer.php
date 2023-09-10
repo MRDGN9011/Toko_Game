e76fb22542d7db64485e8efad4044214bd09c589
E-rapot_code_ignether <dadangk938@gmail.com>
2023-09-10 18:41:24 +0700

    first commit

---
+++ Toko-Game/kelompok2/application/views/layouts/_footer.php
@@ -0,0 +1,10 @@
+<footer class="footer mt-5">
+	<nav class="navbar navbar-dark bg-light sticky-bottom">
+		<div class="container d-flex justify-content-center">
+			<div class="copyright text-info p-3">
+				<small class="text-info p-3">T O K O G A M E
+				</small>
+			</div>
+		</div>
+	</nav>	
+</footer>  
